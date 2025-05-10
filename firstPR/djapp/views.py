e4f794dedcec131b907aa4004693801b2b992c72
um-shuhaib <shuhaib5560@gmail.com>
2025-05-10 13:15:23 +0530

    django template language , variable, tag and filter are familiarised

--- firstPR/djapp/views.py
+++ firstPR/djapp/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 def hello(request):
-    return HttpResponse("hello") 
+    movie_data = {'movie_list': [ {
+        'title':'Ghodha',
+        'summary':'',
+        'year':'2028',
+        'success':False
+    },{
+        'title':'mannan',
+        'summary':'it is a good movie',
+        'year':'2028',
+        'success':True
+    },{
+        'title':'rajav',
+        'summary':'it is a good movie',
+        'year':'2028',
+        'success':True
+    },{
+        'title':'pattini',
+        'summary':'it is a good movie',
+        'year':'2028',
+        'success':False
+    },{
+        'title':'saram',
+        'summary':'it is a good movie',
+        'year':'1990',
+        'success':True
+    },{
+        'title':'pani',
+        'summary':'it is a good movie',
+        'year':'2028',
+        'success':True
+    }]}
+    return render(request,'hello.html',movie_data)
