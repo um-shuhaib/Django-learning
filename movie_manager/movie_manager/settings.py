2e9abd0440fea6053c6ef01b6bcad5d5e23c4921
um-shuhaib <shuhaib5560@gmail.com>
2025-05-21 20:37:49 +0530

    psql Connected successfully

--- movie_manager/movie_manager/settings.py
+++ movie_manager/movie_manager/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'movie_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movie_db',
+        'HOST':'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'PORT': '5432',
     }
 }
 
